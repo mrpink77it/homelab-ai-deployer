2436799d56126b7c324df00d78c7b6facedb69b9
mrpink77it <56739854+mrpink77it@users.noreply.github.com>
2026-08-17 22:18:12 +0200

    Add commands to install and update packages

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+apt install sudo
+sudo apt update
+sudo apt upgrade -y
 sudo apt install git -y
 sudo rm -rf homelab-ai-deployer homelab-ai-logs && \
 git clone https://github.com/mrpink77it/homelab-ai-deployer.git && \
