e371471d23c83f62a5b6e7d6ba7ed4d17ae43bd5
mrpink77it <56739854+mrpink77it@users.noreply.github.com>
2026-08-17 22:23:22 +0200

    Refactor install script for clarity and efficiency
    Removed redundant commands and ensured proper execution of the main script.

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ apt install sudo
 sudo apt update
 sudo apt upgrade -y
 sudo apt install git -y
-sudo rm -rf homelab-ai-deployer homelab-ai-logs && \
-git clone https://github.com/mrpink77it/homelab-ai-deployer.git && \
-cd homelab-ai-deployer && \
-sudo chmod +x *.sh && \
+sudo rm -rf homelab-ai-deployer homelab-ai-logs
+git clone https://github.com/mrpink77it/homelab-ai-deployer.git
+cd homelab-ai-deployer
+sudo chmod +x *.sh
 sudo ./main.sh
