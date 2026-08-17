6b13d2f010d083134c024e157397406e9e9999a2
mrpink77it <56739854+mrpink77it@users.noreply.github.com>
2026-08-17 22:10:43 +0200

    Refactor install.sh to streamline installation process
    Combined multiple commands into a single line for efficiency.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-sudo rm -rf homelab-ai-deployer
-sudo rm -rf homelab-ai-logs
-sudo apt install git -y
-git clone https://github.com/mrpink77it/homelab-ai-deployer.git
-cd homelab-ai-deployer
-sudo chmod +x main.sh manager.sh manager-amd.sh sandbox_setup.sh setup_jupyter.sh monitor.sh
+sudo rm -rf homelab-ai-deployer homelab-ai-logs && \
+git clone https://github.com/mrpink77it/homelab-ai-deployer.git && \
+cd homelab-ai-deployer && \
+sudo chmod +x *.sh && \
 sudo ./main.sh
