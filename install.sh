535fe9be8569e539c82b29cf3f24ac02479b1945
mrpink77it <56739854+mrpink77it@users.noreply.github.com>
2026-08-19 13:33:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-apt install sudo
-sudo apt update
-sudo apt upgrade -y
-sudo apt install git -y
-sudo rm -rf homelab-ai-deployer homelab-ai-logs
+apt update
+apt upgrade -y
+apt install git -y
+rm -rf homelab-ai-deployer homelab-ai-logs
 git clone https://github.com/mrpink77it/homelab-ai-deployer.git
 cd homelab-ai-deployer
-sudo chmod +x *.sh
-sudo ./main.sh
+chmod +x *.sh
+./main.sh
