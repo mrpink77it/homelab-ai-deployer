2dd5fe3276030dd35cce69a39279f22c3374a213
Alessandro Pappone <56739854+mrpink77it@users.noreply.github.com>
2026-08-19 23:26:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update
 apt upgrade -y
-apt install sudo procps pciutils whiptail software-properties-common ca-certificates curl wget git build-essential -y
+apt install sudo procps pciutils whiptail ca-certificates curl wget git build-essential mc -y
 rm -rf homelab-ai-deployer homelab-ai-logs
 git clone https://github.com/mrpink77it/homelab-ai-deployer.git
 cd homelab-ai-deployer
